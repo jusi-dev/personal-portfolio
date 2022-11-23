9705485467f9d7c996d22ba9fe4d99cb9d996fb4
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2022-11-23 20:06:19 +0100

    Trying to fix IOS bug

--- resize.js
+++ resize.js
@@ -1,6 +1,6 @@
-const documentHeight = () => {
-    const doc = document.documentElement
-    doc.style.setProperty('--doc-height', '${window.innerHeight}px')
-}
-window.addEventListener('resize', documentHeight)
-documentHeight()
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
