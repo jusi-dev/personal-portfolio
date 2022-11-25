4153c5a538259f5e5a41a1b45c212b90d361c15d
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2022-11-25 11:42:12 +0100

    Debugging

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const mainnav = document.querySelector(".main-nav");
+const navelem = document.querySelector(".nav-elem");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -9,4 +10,5 @@ hamburger.addEventListener("click", () => {
 document.querySelectorAll(".nav-elem").forEach(n => n.addEventListener("click", () => {
     hamburger.classList.remove("active");
     mainnav.classList.remove("active");
+    navelem.classList.toggle("active");
 }))
