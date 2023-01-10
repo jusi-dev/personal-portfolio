d7edd2db36cc2af2874d0b0fd249b75dd67aeb6a
Justin Winistoerfer <winistoerfer.justin@gmx.ch>
2023-01-10 22:15:13 +0100

    Fixed problems in lightmode and added a few animations

--- script.js
+++ script.js
@@ -8,26 +8,41 @@ const burgerWrapper = document.querySelector("burger-wrapper");
 const aElements = document.querySelectorAll('ul a.nav-elem');
 const downArrow = document.querySelector('.arrow-down');
 const allAElements = document.querySelectorAll('ul a');
+const skillSection = document.querySelector('#skill-section');
 
-const mePart = document.querySelector(".me");
+const startPart = document.querySelector("#start");
+const mePart = document.querySelector("#me");
+const workPart = document.querySelector("#work");
+const contactPart = document.querySelector("#contact");
 const meUl = document.querySelector(".me-ul");
+const workUl = document.querySelector(".work-ul");
+const contactUl = document.querySelector(".contact-ul");
+
+const aboutMe = document.querySelector(".about-me");
 
 const impressumFooter = document.querySelector(".footer-list-a");
 const closePopupBtn = document.querySelector(".close-btn");
 const popupWindow = document.getElementById("popup-1");
 
-const styleSheet = document.styleSheets[0];
 const lightModeBtn = document.querySelector("#lightmode-btn");
 const darkModeBtn = document.querySelector("#darkmode-btn");
 const purple = document.querySelectorAll(".purple");
 const stickyClass = document.querySelector(".sticky");
 const aElement = document.querySelectorAll("a");
+const burgerBar = document.querySelectorAll(".bar");
+const iconRow1 = document.querySelectorAll(".iconRow-1");
+const iconRow2 = document.querySelectorAll(".iconRow-2");
+const iconRow3 = document.querySelectorAll(".iconRow-3");
 
 
 var height = window.innerHeight;
 var bottom = height + window.scrollY;
 
 var lightModeActive = false;
+
+/*                                    */
+        /* TOGGLE LIGHTMODE */
+/*                                    */
 
 lightModeBtn.addEventListener("click", () => {
   lightModeActive = true;
@@ -43,6 +58,10 @@ lightModeBtn.addEventListener("click", () => {
   navelem.forEach(elem => {
     elem.classList.add("lightModeText");
   });
+
+  burgerBar.forEach(elem => {
+    elem.classList.add("burgerLightMode")
+  })
   
   purple.forEach(purple => {
     purple.style.color = '#8d34ff';
@@ -52,6 +71,10 @@ lightModeBtn.addEventListener("click", () => {
     elem.classList.add("lightModeText");
   })
 });
+
+/*                                    */
+        /* TOGGLE DARKMODE */
+/*                                    */
 
 darkModeBtn.addEventListener("click", () => {
   lightModeActive = false;
@@ -77,6 +100,10 @@ darkModeBtn.addEventListener("click", () => {
   })
 })
 
+/*                                    */
+        /* IMPRESSUM MODAL */
+/*                                    */
+
 impressumFooter.addEventListener("click", () => {
   popupWindow.classList.toggle("active");
 });
@@ -84,6 +111,10 @@ impressumFooter.addEventListener("click", () => {
 closePopupBtn.addEventListener("click", () => {
   popupWindow.classList.toggle("active");
 });
+
+/*                                    */
+        /* BURGER MENU */
+/*                                    */
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -95,35 +126,7 @@ document.querySelectorAll(".nav-elem").forEach(n => n.addEventListener("click", 
     mainnav.classList.remove("active");
 }));
 
-downArrow.addEventListener("click", () => {
-    document.body.style.overflowY = "scroll";
-    document.body.style.position = "static";
-});
-
-// Add an event listener that listens for the "scroll" event
-/* window.addEventListener("scroll", function() {
-  if (window.matchMedia("(min-width: 993px)").matches) {
-    if (mePart.getBoundingClientRect().top <= 200) {
-      meUl.style.color = "#9043f6";
-    } else if (mePart.getBoundingClientRect().top <= bottom) {
-      meUl.style.color = "white";
-    }
-  } else if (window.matchMedia("(max-width: 992px)").matches) {
-    if (mePart.getBoundingClientRect().top <= 200) {
-      meUl.style.color = "red";
-    }
-  } else if (window.matchMedia("(max-width: 768px)").matches) {
-    if (mePart.getBoundingClientRect().top <= 200) {
-      meUl.style.color = "red";
-    }
-  } else if (window.matchMedia("(max-width: 600px)").matches) {
-    if (mePart.getBoundingClientRect().top <= 200) {
-      meUl.style.color = "red";
-    }
-  }
-}); */
-
-
+/*
 // loop through all the selected `a` elements
 aElements.forEach(aElement => {
   // attach a `click` event listener to the `a` element
@@ -142,9 +145,13 @@ aElements.forEach(aElement => {
     event.target.style.color = '#9043f6';
   });
 });
+*/
 
+/*                                    */
+        /* SCROLL COLOR NAV */
+/*                                    */
 
-// listen for the scroll event
+// Add the background color to the nav bar after scrolling
 window.addEventListener('scroll', function() {
     var scrollPos = window.scrollY;
     // add a class to the body element to change the background color
@@ -161,3 +168,98 @@ window.addEventListener('scroll', function() {
       navBar.style.background = "none";
     }
   });
+
+/*                                    */
+        /* OBSERVER */
+/*                                    */
+
+// Observer for observing things lol
+const observer = new IntersectionObserver((entries) => {
+  // Check if element is intersecting with the viewport
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      if (entry.target === mePart) {
+        if (lightModeActive === true) {
+          meUl.style.color = '#9043f6';
+          workUl.style.color = '#333333';
+          contactUl.style.color = '#333333';
+        } else if (lightModeActive === false) {
+          meUl.style.color = '#9043f6';
+          workUl.style.color = 'white';
+          contactUl.style.color = 'white';
+        }
+        
+      } else if (entry.target === workPart) {
+        if (lightModeActive === true) {
+          workUl.style.color = '#9043f6';
+          meUl.style.color = '#333333';
+          contactUl.style.color = '#333333';
+        } else if (lightModeActive === false) {
+          workUl.style.color = '#9043f6';
+          contactUl.style.color = 'white';
+          meUl.style.color = 'white';
+        }
+      } else if (entry.target === contactPart) {
+        if (lightModeActive === true) {
+          contactUl.style.color = '#9043f6';
+          workUl.style.color = '#333333';
+          meUl.style.color = '#333333';
+        } else if (lightModeActive === false) {
+          contactUl.style.color = '#9043f6';
+          meUl.style.color = 'white';
+          workUl.style.color = 'white';
+        }
+        
+      } else if (entry.target === startPart) {
+        if (lightModeActive === true) {
+          meUl.style.color = '#333333';
+          workUl.style.color = '#333333';
+          contactUl.style.color = '#333333';
+        } else if (lightModeActive === false) {
+          contactUl.style.color = 'white';
+          meUl.style.color = 'white';
+          workUl.style.color = 'white';
+        }
+      }
+    }
+  }
+}, {
+  rootMargin: '-50%'
+});
+
+const iconObserver = new IntersectionObserver((entries) => {
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate__fadeInUp');
+    }
+  }
+}, {
+  rootMargin: '1%'
+});
+
+const animationObserver = new IntersectionObserver((entries) => {
+  // Check if element is intersecting with the viewport
+  for (const entry of entries) {
+    if (entry.isIntersecting) {
+      if (entry.target === mePart) {
+        entry.target.classList.add('animate__fadeIn')
+        mePart.style.visibility = 'visible';
+      }
+    }
+  }
+}, {
+  rootMargin: '-40%'
+});
+
+
+observer.observe(startPart);
+observer.observe(mePart);
+observer.observe(workPart);
+observer.observe(contactPart);
+
+console.log("Test")
+iconRow1.forEach(element => iconObserver.observe(element));
+iconRow2.forEach(element => iconObserver.observe(element));
+iconRow3.forEach(element => iconObserver.observe(element));
+
+animationObserver.observe(mePart);
